148a6e56e6306c05802676f15eb514ff71d07f74
Demon1997rus <rotanov.1997@mail.ru>
2024-08-28 08:01:01 -0700

    add function qt_add_library

---
+++ cmake/qt_add_library.cmake
@@ -0,0 +1,21 @@
+# Определение функции qt_add_library
+function(qt_add_library TARGET_NAME)
+    # Получаем все header и cpp файлы на текущем уровне каталога и ниже
+    file(GLOB_RECURSE HEADER_FILES "*.h" "*.hpp")
+    file(GLOB_RECURSE SOURCE_FILES "*.cpp" "*.cxx" "*.cc")
+
+    # Добавляем библиотеку
+    add_library(${TARGET_NAME} ${HEADER_FILES} ${SOURCE_FILES})
+
+    # Получаем остальные аргументы - список библиотек
+    set(LIBS ${ARGN})
+    if(DEFINED QT_LIBRARIES_FOUND)
+        set(LIBS ${LIBS} ${QT_LIBRARIES_FOUND})
+    endif()
+
+    # Цикл для добавления каждой библиотеки
+    foreach(LIB ${LIBS})
+        target_link_libraries(${TARGET_NAME} PRIVATE ${LIB})
+    endforeach()
+
+endfunction()
