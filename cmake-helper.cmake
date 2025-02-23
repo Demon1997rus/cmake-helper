81d083b0289ea305c72651282352c9bbc2b258e1
Demon1997rus <rotanov.1997@mail.ru>
2025-02-23 20:48:15 +0300

    include directories

--- cmake-helper.cmake
+++ cmake-helper.cmake
@@ -13,6 +13,7 @@ set(CMAKE_AUTOMOC ON)
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/../lib)
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/../lib)
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/../bin)
+include_directories(${CMAKE_BINARY_DIR}/../include)
 
 # Add cmake functions
 file(GLOB_RECURSE CMAKE_HELPER_FUNCTIONS cmake-helper/cmake/*.cmake*)
